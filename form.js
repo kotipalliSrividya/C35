2b86ce1cf2ee820b821e472738533630b89e4a4e
kotipalliSrividya <63097838+kotipalliSrividya@users.noreply.github.com>
2020-05-27 14:18:06 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,33 @@
+class Form{
+    constructor(){}
+    display(){
+        var title=createElement('h2');
+        title.html("Car Racing Game")
+        title.position(130,0);
+         
+        var button= createButton('h3');
+        button.html("PLAY");
+        button.position(250,200);
+
+        var input=createInput("Name");
+        input.position(150,170);
+
+        var greetings=createElement('h2')
+       
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var nameEntered=input.value();
+            player.update(nameEntered);
+            playerCount=playerCount+1;
+             player.updateCount(playerCount);
+            
+
+            greetings.html("Hello! "+nameEntered);
+            greetings.position(250,300);
+
+
+        })
+    }
+}
---
+++ game.js
@@ -0,0 +1,22 @@
+class Game{
+    constructor(){}
+
+    getState(){
+        var gameStateRef=database.ref('gameState');
+        gameStateRef.on("value",function(data){
+            gameState=data.val();
+        })
+    }
+    update(state){
+       database.ref('gameState').set({
+        gameState:state
+       });
+
+    }
+    start(){
+        form=new Form();
+        form.display();
+        player=new Player();
+        player.getCount();
+    }
+}
